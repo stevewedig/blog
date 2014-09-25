ab6fe44cd4deedf85732ccac1655e6cf8f77f00f
Steve Wedig <stevewedig@gmail.com>
2014-09-25 15:03:22 -0700

    id graph lib javadoc

--- src/main/java/com/stevewedig/blog/digraph/id_graph/IdDagLib.java
+++ src/main/java/com/stevewedig/blog/digraph/id_graph/IdDagLib.java
@@ -14,29 +14,44 @@ public abstract class IdDagLib {
   // fromParentMap
   // ===========================================================================
 
+  /**
+   * Create an IdDag from the dag's id set and a mapping from id to parent ids (specified as a multimap).
+   */
   public static <Id> IdDag<Id> fromParentMap(Set<Id> idSet, Multimap<Id, Id> id__parentIds) {
     return new IdDagClass<Id>(ImmutableSet.copyOf(idSet),
         ImmutableSetMultimap.copyOf(id__parentIds));
   }
 
+  /**
+   * Create an IdDag from a mapping from id to parent ids (specified as a multimap), assuming all ids are in that mapping.
+   */
   public static <Id> IdDag<Id> fromParentMap(Multimap<Id, Id> id__parentIds) {
     return fromParentMap(MultimapLib.keysAndValues(id__parentIds), id__parentIds);
   }
 
+  /**
+   * Create an IdDag from the dag's id set and a mapping from id to parent ids (specified as alternating keys and values).
+   */
   @SafeVarargs
-  public static <Id> IdDag<Id> fromParentMap(Id... entries) {
-    return fromParentMap(MultimapLib.of(entries));
+  public static <Id> IdDag<Id> fromParentMap(Set<Id> idSet, Id... alternatingIdsAndParentIds) {
+    return fromParentMap(idSet, MultimapLib.of(alternatingIdsAndParentIds));
   }
 
+  /**
+   * Create an IdDag from a mapping from id to parent ids (specified as alternating keys and values), assuming all ids are in that mapping.
+   */
   @SafeVarargs
-  public static <Id> IdDag<Id> fromParentMap(Set<Id> idSet, Id... entries) {
-    return fromParentMap(idSet, MultimapLib.of(entries));
+  public static <Id> IdDag<Id> fromParentMap(Id... alternatingIdsAndParentIds) {
+    return fromParentMap(MultimapLib.of(alternatingIdsAndParentIds));
   }
 
   // ===========================================================================
   // fromChildMap
   // ===========================================================================
 
+  /**
+   * Create an IdDag from the dag's id set and a mapping from id to child ids (specified as a multimap).
+   */
   public static <Id> IdDag<Id> fromChildMap(Set<Id> idSet, Multimap<Id, Id> id__childIds) {
 
     ImmutableSetMultimap<Id, Id> id__parentIds =
@@ -45,18 +60,27 @@ public abstract class IdDagLib {
     return fromParentMap(idSet, id__parentIds);
   }
 
+  /**
+   * Create an IdDag from a mapping from id to child ids (specified as a multimap), assuming all ids are in that mapping.
+   */
   public static <Id> IdDag<Id> fromChildMap(Multimap<Id, Id> id__childIds) {
     return fromChildMap(MultimapLib.keysAndValues(id__childIds), id__childIds);
   }
 
+  /**
+   * Create an IdDag from the dag's id set and a mapping from id to child ids (specified as alternating keys and values).
+   */
   @SafeVarargs
-  public static <Id> IdDag<Id> fromChildMap(Id... entries) {
-    return fromChildMap(MultimapLib.of(entries));
+  public static <Id> IdDag<Id> fromChildMap(Set<Id> idSet, Id... alternatingIdsAndChildIds) {
+    return fromChildMap(idSet, MultimapLib.of(alternatingIdsAndChildIds));
   }
 
+  /**
+   * Create an IdDag from a mapping from id to child ids (specified as alternating keys and values), assuming all ids are in that mapping.
+   */
   @SafeVarargs
-  public static <Id> IdDag<Id> fromChildMap(Set<Id> idSet, Id... entries) {
-    return fromChildMap(idSet, MultimapLib.of(entries));
+  public static <Id> IdDag<Id> fromChildMap(Id... alternatingIdsAndChildIds) {
+    return fromChildMap(MultimapLib.of(alternatingIdsAndChildIds));
   }
 
 }
--- src/main/java/com/stevewedig/blog/digraph/id_graph/IdTreeLib.java
+++ src/main/java/com/stevewedig/blog/digraph/id_graph/IdTreeLib.java
@@ -14,29 +14,44 @@ public abstract class IdTreeLib {
   // fromParentMap
   // ===========================================================================
 
+  /**
+   * Create an IdTree from the tree's id set and a mapping from id to parent ids (specified as a multimap).
+   */
   public static <Id> IdTree<Id> fromParentMap(Set<Id> idSet, Multimap<Id, Id> id__parentIds) {
     return new IdTreeClass<Id>(ImmutableSet.copyOf(idSet),
         ImmutableSetMultimap.copyOf(id__parentIds));
   }
 
+  /**
+   * Create an IdTree from a mapping from id to parent ids (specified as a multimap), assuming all ids are in that mapping.
+   */
   public static <Id> IdTree<Id> fromParentMap(Multimap<Id, Id> id__parentIds) {
     return fromParentMap(MultimapLib.keysAndValues(id__parentIds), id__parentIds);
   }
 
+  /**
+   * Create an IdTree from the tree's id set and a mapping from id to parent ids (specified as alternating keys and values).
+   */
   @SafeVarargs
-  public static <Id> IdTree<Id> fromParentMap(Id... entries) {
-    return fromParentMap(MultimapLib.of(entries));
+  public static <Id> IdTree<Id> fromParentMap(Set<Id> idSet, Id... alternatingIdsAndParentIds) {
+    return fromParentMap(idSet, MultimapLib.of(alternatingIdsAndParentIds));
   }
 
+  /**
+   * Create an IdTree from a mapping from id to parent ids (specified as alternating keys and values), assuming all ids are in that mapping.
+   */
   @SafeVarargs
-  public static <Id> IdTree<Id> fromParentMap(Set<Id> idSet, Id... entries) {
-    return fromParentMap(idSet, MultimapLib.of(entries));
+  public static <Id> IdTree<Id> fromParentMap(Id... alternatingIdsAndParentIds) {
+    return fromParentMap(MultimapLib.of(alternatingIdsAndParentIds));
   }
 
   // ===========================================================================
   // fromChildMap
   // ===========================================================================
 
+  /**
+   * Create an IdTree from the tree's id set and a mapping from id to child ids (specified as a multimap).
+   */
   public static <Id> IdTree<Id> fromChildMap(Set<Id> idSet, Multimap<Id, Id> id__childIds) {
 
     ImmutableSetMultimap<Id, Id> id__parentIds =
@@ -45,18 +60,27 @@ public abstract class IdTreeLib {
     return fromParentMap(idSet, id__parentIds);
   }
 
+  /**
+   * Create an IdTree from a mapping from id to child ids (specified as a multimap), assuming all ids are in that mapping.
+   */
   public static <Id> IdTree<Id> fromChildMap(Multimap<Id, Id> id__childIds) {
     return fromChildMap(MultimapLib.keysAndValues(id__childIds), id__childIds);
   }
 
+  /**
+   * Create an IdTree from the tree's id set and a mapping from id to child ids (specified as alternating keys and values).
+   */
   @SafeVarargs
-  public static <Id> IdTree<Id> fromChildMap(Id... entries) {
-    return fromChildMap(MultimapLib.of(entries));
+  public static <Id> IdTree<Id> fromChildMap(Set<Id> idSet, Id... alternatingIdsAndChildIds) {
+    return fromChildMap(idSet, MultimapLib.of(alternatingIdsAndChildIds));
   }
 
+  /**
+   * Create an IdTree from a mapping from id to child ids (specified as alternating keys and values), assuming all ids are in that mapping.
+   */
   @SafeVarargs
-  public static <Id> IdTree<Id> fromChildMap(Set<Id> idSet, Id... entries) {
-    return fromChildMap(idSet, MultimapLib.of(entries));
+  public static <Id> IdTree<Id> fromChildMap(Id... alternatingIdsAndChildIds) {
+    return fromChildMap(MultimapLib.of(alternatingIdsAndChildIds));
   }
 
 }
