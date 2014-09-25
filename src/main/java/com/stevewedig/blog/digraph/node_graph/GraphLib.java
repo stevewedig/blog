4482d4122a13a41fb66c58ff5fdb6d2e6a1880f2
Steve Wedig <stevewedig@gmail.com>
2014-09-25 15:11:47 -0700

    graph lib javadoc

--- src/main/java/com/stevewedig/blog/digraph/node_graph/GraphLib.java
+++ src/main/java/com/stevewedig/blog/digraph/node_graph/GraphLib.java
@@ -8,7 +8,7 @@ import com.stevewedig.blog.digraph.node.*;
 import com.stevewedig.blog.util.SetLib;
 
 /**
- * A library for creating Graphs.
+ * A library for creating Graphs (digraphs containing nodes).
  */
 public abstract class GraphLib {
 
@@ -16,6 +16,9 @@ public abstract class GraphLib {
   // graph from state
   // ===========================================================================
 
+  /**
+   * Create a Graph the nested IdGraph (the arc structure) and the mapping between id and node.
+   */
   public static <Id, Node> Graph<Id, Node> graph(IdGraph<Id> idGraph,
       ImmutableBiMap<Id, Node> id__node) {
 
@@ -26,6 +29,9 @@ public abstract class GraphLib {
   // graph from up nodes
   // ===========================================================================
 
+  /**
+   * Create a Graph from an UpNode set.
+   */
   public static <Id, Node extends UpNode<Id>> Graph<Id, Node> up(ImmutableSet<Node> nodeSet) {
 
     IdGraph<Id> idGraph = UpNodeLib.nodes__idGraph(nodeSet);
@@ -35,15 +41,25 @@ public abstract class GraphLib {
     return graph(idGraph, id__node);
   }
 
-  public static <Id, Node extends UpNode<Id>> Graph<Id, Node> up(Iterable<Node> nodeIter) {
-    return up(ImmutableSet.copyOf(nodeIter));
+  /**
+   * Create a Graph from an UpNode iterable.
+   */
+  public static <Id, Node extends UpNode<Id>> Graph<Id, Node> up(Iterable<Node> nodeIterable) {
+    return up(ImmutableSet.copyOf(nodeIterable));
   }
 
+  /**
+   * Create a Graph from an UpNode array.
+   */
   @SafeVarargs
   public static <Id, Node extends UpNode<Id>> Graph<Id, Node> up(Node... nodeArray) {
     return up(ImmutableSet.copyOf(nodeArray));
   }
 
+  /**
+   * Create a Graph from the union of multiple UpNode sets (remember that Graph&lt;Node&gt; extends
+   * Set&lt;Node&gt;).
+   */
   @SafeVarargs
   public static <Id, Node extends UpNode<Id>> Graph<Id, Node> up(Set<Node>... graphs) {
     return up(SetLib.union(graphs));
@@ -53,6 +69,9 @@ public abstract class GraphLib {
   // graph from down nodes
   // ===========================================================================
 
+  /**
+   * Create a Graph from an DownNode set.
+   */
   public static <Id, Node extends DownNode<Id>> Graph<Id, Node> down(ImmutableSet<Node> nodeSet) {
 
     IdGraph<Id> idGraph = DownNodeLib.nodes__idGraph(nodeSet);
@@ -62,15 +81,25 @@ public abstract class GraphLib {
     return graph(idGraph, id__node);
   }
 
-  public static <Id, Node extends DownNode<Id>> Graph<Id, Node> down(Iterable<Node> nodeIter) {
-    return down(ImmutableSet.copyOf(nodeIter));
+  /**
+   * Create a Graph from an DownNode iterable.
+   */
+  public static <Id, Node extends DownNode<Id>> Graph<Id, Node> down(Iterable<Node> nodeIterable) {
+    return down(ImmutableSet.copyOf(nodeIterable));
   }
 
+  /**
+   * Create a Graph from an DownNode array.
+   */
   @SafeVarargs
   public static <Id, Node extends DownNode<Id>> Graph<Id, Node> down(Node... nodeArray) {
     return down(ImmutableSet.copyOf(nodeArray));
   }
 
+  /**
+   * Create a Graph from the union of multiple DownNode sets (remember that Graph&lt;Node&gt;
+   * extends Set&lt;Node&gt;).
+   */
   @SafeVarargs
   public static <Id, Node extends DownNode<Id>> Graph<Id, Node> down(Set<Node>... graphs) {
     return down(SetLib.union(graphs));
